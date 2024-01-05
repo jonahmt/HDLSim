4c8b6c86f4bfcd711f4a0c81f485c76383ec7586
jonahmt <jonah.tharakan@gmail.com>
2024-01-04 21:41:46 -0800

    Added logical and bitwise not operators, which work properly

--- src/Expression.java
+++ src/Expression.java
@@ -9,8 +9,18 @@ public class Expression {
     }
 
     public int eval(HashMap<String, Integer> values) {
+        char firstChar = expression.charAt(0);
+
+        // Not case
+        if (firstChar == '!') {
+            return evalLogicalNot(values);
+        }
+        else if (firstChar == '~') {
+            return evalBitwiseNot(values);
+        }
+
         // Subexpression case
-        if (expression.contains("(")) {
+        else if (expression.contains("(")) {
 
         }
 
@@ -30,4 +40,27 @@ public class Expression {
     public int eval() {
         return eval(new HashMap<>());
     }
+
+
+    // PRIVATE HELPER METHODS ///////////////////////////////////////////
+
+    /**
+     * Evaluates the subexpression and then returns the logical not (!)
+     * of the subexpression value
+     */
+    private int evalLogicalNot(HashMap<String, Integer> values) {
+        Expression subexpr = new Expression(expression.substring(1));
+        return subexpr.eval(values) == 0 ? 1 : 0;
+    }
+
+    /**
+     * Evaluates the subexpression and then returns the bitwise not (~)
+     * of the subexpression value
+     */
+    private int evalBitwiseNot(HashMap<String, Integer> values) {
+        Expression subexpr = new Expression(expression.substring(1));
+        return ~subexpr.eval(values);
+    }
+
+
 }
--- test/ExpressionTest.java
+++ test/ExpressionTest.java
@@ -20,11 +20,53 @@ class ExpressionTest {
         bindings.put("A", 0);
         bindings.put("B", 12);
         bindings.put("hello", 345);
-        bindings.put("l33tc0d3", 6789);
 
         assertEquals(0, (new Expression("A").eval(bindings)));
         assertEquals(12, (new Expression("B").eval(bindings)));
         assertEquals(345, (new Expression("hello").eval(bindings)));
         assertEquals(6789, (new Expression("l33tc0d3").eval(bindings)));
     }
+
+    @Test
+    public void basicLogicalNotTest() {
+        HashMap<String, Integer> bindings = new HashMap<>();
+        bindings.put("A", 0);
+        bindings.put("B", 0xFFFF_0000);
+
+        Expression exp1 = new Expression("!0");
+        Expression exp2 = new Expression("!1");
+        Expression exp3 = new Expression("!!0");
+        Expression exp4 = new Expression("!!!!!!!!!0"); // 9 !
+        Expression exp5 = new Expression("!A");
+        Expression exp6 = new Expression("!B");
+
+        assertEquals(1, exp1.eval(bindings));
+        assertEquals(0, exp2.eval(bindings));
+        assertEquals(0, exp3.eval(bindings));
+        assertEquals(1, exp4.eval(bindings));
+        assertEquals(1, exp5.eval(bindings));
+        assertEquals(0, exp6.eval(bindings));
+    }
+
+    @Test
+    public void basicBitwiseNotTest() {
+        HashMap<String, Integer> bindings = new HashMap<>();
+        bindings.put("A", 0);
+        bindings.put("B", 0xFFFF_0000);
+
+        Expression exp1 = new Expression("~0");
+        Expression exp2 = new Expression("~1");
+        Expression exp3 = new Expression("~15");
+        Expression exp4 = new Expression("~~69");
+        Expression exp5 = new Expression("~A");
+        Expression exp6 = new Expression("~B");
+
+        assertEquals(0xFFFF_FFFF, exp1.eval(bindings));
+        assertEquals(0xFFFF_FFFE, exp2.eval(bindings));
+        assertEquals(0xFFFF_FFF0, exp3.eval(bindings));
+        assertEquals(69, exp4.eval(bindings));
+        assertEquals(0xFFFF_FFFF, exp5.eval(bindings));
+        assertEquals(0x0000_FFFF, exp6.eval(bindings));
+    }
+
 }
